d668b8117ac8d4406c0f123d6c5e16c4549291cc
ShinYonghwan474 <43164033+ShinYonghwan474@users.noreply.github.com>
2026-08-24 07:18:36 +0900

    Update gen_2_families.h

--- src/data/pokemon/species_info/gen_2_families.h
+++ src/data/pokemon/species_info/gen_2_families.h
@@ -5721,7 +5721,8 @@ const struct SpeciesInfo gSpeciesInfoGen2[] =
         .formSpeciesIdTable = sSneaselFormSpeciesIdTable,
     #if P_GEN_4_CROSS_EVOS
         .evolutions = EVOLUTION({EVO_LEVEL, 0, SPECIES_WEAVILE, CONDITIONS({IF_TIME, TIME_NIGHT}, {IF_HOLD_ITEM, ITEM_RAZOR_CLAW})},
-                                {EVO_ITEM, ITEM_RAZOR_CLAW, SPECIES_WEAVILE, CONDITIONS({IF_TIME, TIME_NIGHT})}),
+                                {EVO_ITEM, ITEM_RAZOR_CLAW, SPECIES_WEAVILE, CONDITIONS({IF_TIME, TIME_NIGHT})},
+                                {EVO_ITEM, ITEM_DAWN_STONE, SPECIES_SNEASEL_HISUI}),
     #endif
     },
 
